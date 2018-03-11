368f0b37197999ee703a60f969a0b3eec29b079b
Ankur Gupta <guptaankur.gupta0@gmail.com>
2018-03-11 19:47:16 +0530

    Implement login page

---
+++ src/Components/LoginForm.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import '../LoginForm.css';
+import 'font-awesome/css/font-awesome.min.css';
+import waterBackground from '../images/backgroundWater.jpg';
+
+class LoginForm extends Component {
+
+  submitForm(){
+    /*TODO Implement this function*/
+    console.log("Form Submitted!");
+  }
+
+  render() {
+    const bgStyle = {
+      backgroundImage: 'url(' + waterBackground + ')',
+      backgroundColor: 'rgb(255, 255, 255)'
+    };
+    
+    return (
+      <div className="LoginForm">
+          <div style={bgStyle}>
+              <div className="form-card-container">
+                  <div className="form-card">
+                      <div className="form-wrapper">
+                          <form className="login-form" onSubmit={this.submitForm}>
+                              <span className="login-form-logo">
+                              <i className="fa fa-tint"></i>
+                              </span>
+
+                              <span className="login-form-title">
+                                  Log In
+                              </span>
+
+                              <div className="input-wrapper validate-input" data-validate="Enter Username">
+                                  <i className="fa fa-user"></i>
+                                  <input className="input-field" type="text" name="Username" placeholder="Username"/>
+                              </div>
+
+                              <div className="input-wrapper validate-input" data-validate="Enter Password">
+                                  <i className="fa fa-lock"></i>
+                                  <input className="input-field" type="password" name="Password" placeholder="Password"/>
+                              </div>
+
+                              <div className="button-wrapper">
+                						      <button className="button-field">
+                							        Login
+                						      </button>
+                					    </div>
+                          </form>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
