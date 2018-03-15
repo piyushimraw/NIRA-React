c0922ee6b5c3485612d99e5680671b5d201cbcf4
piyushimraw <piyush.srivastavaimraw@gmail.com>
2018-03-15 20:20:28 +0530

    minor changes

--- src/Components/dashboard.jsx
+++ src/Components/dashboard.jsx
@@ -23,23 +23,52 @@ class Dashboard extends Component {
 
   render() {
     const img = this.state.samples.map(sample => {
-      return <ImageCard src={sample.Image} state={sample.State} river={sample.River} time={sample.Time_Of_Sample}/>;
+      return <ImageCard src={sample.Image} 
+                        state={sample.State} 
+                        river={sample.River} 
+                        time={sample.Time_Of_Sample}/>;
     });
     return (
      <div className="container-dashboard">
         <Grid>
           <Row>
-            <Col sm={6} md={9} className="component-card">
+            <Col sm={6} md={12} className="component-card">
               <h1>Map Card</h1>
               <br />
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elit nulla, facilisis nec facilisis eu, viverra fermentum nunc. Proin ut porttitor neque. Etiam eu libero viverra, sagittis dolor a, fermentum ex. In eu vulputate sapien. Ut dapibus augue ac ipsum lacinia tincidunt. Maecenas id imperdiet tellus. Duis sem ipsum, consequat molestie risus in, convallis facilisis dui. In arcu eros, sollicitudin ut magna eu, tempus ullamcorper purus. Sed pretium posuere tellus sit amet sodales. Pellentesque nisl odio, faucibus a placerat non, placerat at orci. Nam eget turpis varius, blandit lectus vel, pretium velit. Suspendisse sagittis convallis ipsum, eu malesuada enim. Curabitur feugiat pellentesque volutpat. Vestibulum mauris sem, vestibulum vitae lectus eu, faucibus finibus velit. Vestibulum sed egestas metus, sit amet sodales odio. Aliquam nibh eros, viverra in ultricies sit amet, mollis id justo.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+               Donec elit nulla, facilisis nec facilisis eu, viverra fermentum nunc. 
+               Proin ut porttitor neque. Etiam eu libero viverra, sagittis dolor a, fermentum ex. 
+               In eu vulputate sapien. Ut dapibus augue ac ipsum lacinia tincidunt. 
+               Maecenas id imperdiet tellus. 
+               Duis sem ipsum, consequat molestie risus in, convallis facilisis dui. 
+               In arcu eros, sollicitudin ut magna eu, tempus ullamcorper purus. 
+               Sed pretium posuere tellus sit amet sodales. 
+               Pellentesque nisl odio, faucibus a placerat non, placerat at orci. 
+               Nam eget turpis varius, blandit lectus vel, pretium velit.
+               Suspendisse sagittis convallis ipsum, eu malesuada enim. 
+               Curabitur feugiat pellentesque volutpat. 
+               Vestibulum mauris sem, vestibulum vitae lectus eu, faucibus finibus velit. 
+               Vestibulum sed egestas metus, sit amet sodales odio. 
+               Aliquam nibh eros, viverra in ultricies sit amet, mollis id justo.
             </Col>
             <Col sm={6} md={3} className="component-card">
               <h1>Info Card</h1>
               <br />
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elit nulla, facilisis nec facilisis eu, viverra fermentum nunc. Proin ut porttitor neque. Etiam eu libero viverra, sagittis dolor a, fermentum ex. In eu vulputate sapien. Ut dapibus augue ac ipsum lacinia tincidunt. Maecenas id imperdiet tellus. Duis sem ipsum, consequat molestie risus in, convallis facilisis dui. In arcu eros, sollicitudin ut magna eu, tempus ullamcorper purus. Sed pretium posuere tellus sit amet sodales. Pellentesque nisl odio, faucibus a placerat non, placerat at orci. Nam eget turpis varius, blandit lectus vel, pretium velit. Suspendisse sagittis convallis ipsum, eu malesuada enim. Curabitur feugiat pellentesque volutpat. Vestibulum mauris sem, vestibulum vitae lectus eu, faucibus finibus velit. Vestibulum sed egestas metus, sit amet sodales odio. Aliquam nibh eros, viverra in ultricies sit amet, mollis id justo.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+              Donec elit nulla, facilisis nec facilisis eu, viverra fermentum nunc. 
+              Proin ut porttitor neque. Etiam eu libero viverra, sagittis dolor a, fermentum ex. 
+              In eu vulputate sapien. Ut dapibus augue ac ipsum lacinia tincidunt. 
+              Maecenas id imperdiet tellus. 
+              Duis sem ipsum, consequat molestie risus in, convallis facilisis dui. 
+              In arcu eros, sollicitudin ut magna eu, tempus ullamcorper purus. 
+              Sed pretium posuere tellus sit amet sodales. 
+              Pellentesque nisl odio, faucibus a placerat non, placerat at orci. 
+              Nam eget turpis varius, blandit lectus vel, pretium velit. 
+              Suspendisse sagittis convallis ipsum, eu malesuada enim. Curabitur feugiat pellentesque volutpat. 
+              Vestibulum mauris sem, vestibulum vitae lectus eu, faucibus finibus velit. 
+              Vestibulum sed egestas metus, sit amet sodales odio. Aliquam nibh eros, viverra in ultricies sit amet, mollis id justo.
             </Col>
-            <Col sm={6} md={6} className="component-card">
+            <Col sm={6} md={3} className="component-card">
               <h1>Pie Chart and Line Graph</h1>
               <br />
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elit nulla, facilisis nec facilisis eu, viverra fermentum nunc. Proin ut porttitor neque. Etiam eu libero viverra, sagittis dolor a, fermentum ex. In eu vulputate sapien. Ut dapibus augue ac ipsum lacinia tincidunt. Maecenas id imperdiet tellus. Duis sem ipsum, consequat molestie risus in, convallis facilisis dui. In arcu eros, sollicitudin ut magna eu, tempus ullamcorper purus. Sed pretium posuere tellus sit amet sodales. Pellentesque nisl odio, faucibus a placerat non, placerat at orci. Nam eget turpis varius, blandit lectus vel, pretium velit. Suspendisse sagittis convallis ipsum, eu malesuada enim. Curabitur feugiat pellentesque volutpat. Vestibulum mauris sem, vestibulum vitae lectus eu, faucibus finibus velit. Vestibulum sed egestas metus, sit amet sodales odio. Aliquam nibh eros, viverra in ultricies sit amet, mollis id justo.
