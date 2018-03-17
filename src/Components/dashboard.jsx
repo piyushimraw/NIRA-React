3b0afe3ea31701202c46b8bec9226df8f4f0822c
Shadowslayer97 <srirangsathyan17@gmail.com>
2018-03-17 12:29:37 +0530

    Added map object and some css to it

--- src/Components/dashboard.jsx
+++ src/Components/dashboard.jsx
@@ -6,6 +6,7 @@ import LineChart from './charts/line_chart/LineChart';
 import PieChart from "./charts/pie_chart/PieChart";
 import Searchbar from "./Searchbar";
 import SampleStore from '../stores/SamplesStore';
+import SimpleMap from "./map/Map"
 
 
 import ImageCard from "./image_card/image_card";
@@ -43,52 +44,56 @@ class Dashboard extends Component {
 
   render() {
     const img = this.state.samples.map(sample => {
-      return <ImageCard src={sample.Image} 
-                        state={sample.State} 
-                        river={sample.River} 
+      return <ImageCard src={sample.Image}
+                        state={sample.State}
+                        river={sample.River}
                         time={sample.Time_Of_Sample}/>;
     });
     return (
 
      <div className="container-dashboard">
+
        <Searchbar/>
         <Grid>
           <Row>
             <Col sm={6} md={5} className="component-card">
               <h1>Map Card</h1>
               <br />
-              {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-               Donec elit nulla, facilisis nec facilisis eu, viverra fermentum nunc. 
-               Proin ut porttitor neque. Etiam eu libero viverra, sagittis dolor a, fermentum ex. 
-               In eu vulputate sapien. Ut dapibus augue ac ipsum lacinia tincidunt. 
-               Maecenas id imperdiet tellus. 
-               Duis sem ipsum, consequat molestie risus in, convallis facilisis dui. 
-               In arcu eros, sollicitudin ut magna eu, tempus ullamcorper purus. 
-               Sed pretium posuere tellus sit amet sodales. 
-               Pellentesque nisl odio, faucibus a placerat non, placerat at orci. 
-               Nam eget turpis varius, blandit lectus vel, pretium velit.
-               Suspendisse sagittis convallis ipsum, eu malesuada enim. 
-               Curabitur feugiat pellentesque volutpat. 
-               Vestibulum mauris sem, vestibulum vitae lectus eu, faucibus finibus velit. 
-               Vestibulum sed egestas metus, sit amet sodales odio. 
-               Aliquam nibh eros, viverra in ultricies sit amet, mollis id justo. */}
+                <SimpleMap />
+              <p>Helow world</p>
+
             </Col>
             <Col sm={6} md={5} className="component-card">
               <h1>Info Card</h1>
               <br />
-              {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Donec elit nulla, facilisis nec facilisis eu, viverra fermentum nunc. 
-              Proin ut porttitor neque. Etiam eu libero viverra, sagittis dolor a, fermentum ex. 
-              In eu vulputate sapien. Ut dapibus augue ac ipsum lacinia tincidunt. 
-              Maecenas id imperdiet tellus. 
-              Duis sem ipsum, consequat molestie risus in, convallis facilisis dui. 
-              In arcu eros, sollicitudin ut magna eu, tempus ullamcorper purus. 
-              Sed pretium posuere tellus sit amet sodales. 
-              Pellentesque nisl odio, faucibus a placerat non, placerat at orci. 
-              Nam eget turpis varius, blandit lectus vel, pretium velit. 
-              Suspendisse sagittis convallis ipsum, eu malesuada enim. Curabitur feugiat pellentesque volutpat. 
-              Vestibulum mauris sem, vestibulum vitae lectus eu, faucibus finibus velit. 
-              Vestibulum sed egestas metus, sit amet sodales odio. Aliquam nibh eros, viverra in ultricies sit amet, mollis id justo. */}
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec elit nulla, facilisis nec facilisis eu, viverra fermentum nunc.
+              Proin ut porttitor neque. Etiam eu libero viverra, sagittis dolor a, fermentum ex.
+              In eu vulputate sapien. Ut dapibus augue ac ipsum lacinia tincidunt.
+              Maecenas id imperdiet tellus.
+              Duis sem ipsum, consequat molestie risus in, convallis facilisis dui.
+              In arcu eros, sollicitudin ut magna eu, tempus ullamcorper purus.
+              Sed pretium posuere tellus sit amet sodales.
+              Pellentesque nisl odio, faucibus a placerat non, placerat at orci.
+              Nam eget turpis varius, blandit lectus vel, pretium velit.
+              Suspendisse sagittis convallis ipsum, eu malesuada enim. Curabitur feugiat pellentesque volutpat.
+              Vestibulum mauris sem, vestibulum vitae lectus eu, faucibus finibus velit.
+              Vestibulum sed egestas metus, sit amet sodales odio. Aliquam nibh eros, viverra in ultricies sit amet, mollis id justo.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+             Donec elit nulla, facilisis nec facilisis eu, viverra fermentum nunc.
+             Proin ut porttitor neque. Etiam eu libero viverra, sagittis dolor a, fermentum ex.
+             In eu vulputate sapien. Ut dapibus augue ac ipsum lacinia tincidunt.
+             Maecenas id imperdiet tellus.
+             Duis sem ipsum, consequat molestie risus in, convallis facilisis dui.
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec elit nulla, facilisis nec facilisis eu, viverra fermentum nunc.
+            Proin ut porttitor neque. Etiam eu libero viverra, sagittis dolor a, fermentum ex.
+            In eu vulputate sapien. Ut dapibus augue ac ipsum lacinia tincidunt.
+            Maecenas id imperdiet tellus.
+            Duis sem ipsum, consequat molestie risus in, convallis facilisis dui.
+            In arcu eros, sollicitudin ut magna eu, tempus ullamcorper purus.
+          
+
             </Col>
 
             <Col sm={6} md={5} className="component-card">
@@ -106,6 +111,8 @@ class Dashboard extends Component {
             </Col>
           </Row>
         </Grid>
+
+
      </div>
     )
   }
---
+++ src/Components/map/Map.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import "./map.css";
+
+const AnyReactComponent = ({ text }) => <div className="sample-text">{text}<div className="sample-point"></div></div>;
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {lat: 59.95, lng: 30.33},
+    zoom: 13
+  };
+
+  render() {
+    return (
+      <div style={{height: '400px', width: '400px'}}>
+      <GoogleMapReact
+        defaultCenter={this.props.center}
+        defaultZoom={this.props.zoom}
+        heatmapLibrary={true}
+    heatmap={{
+      positions: [
+        {
+          lat: 59.955413,
+          lng: 30.337844,
+        },
+        {
+          lat: 59.959100,
+          lng: 30.338676,
+        },
+        {
+          lat:59.957912,
+          lng: 30.335901,
+        },
+      ],
+      options: {
+        radius: 20,
+        opacity: 0.7,
+        gradient: [
+          'rgba(0, 255, 255, 0)',
+          'rgba(0, 255, 255, 1)',
+          'rgba(0, 191, 255, 1)',
+          'rgba(0, 127, 255, 1)',
+          'rgba(0, 63, 255, 1)',
+          'rgba(0, 0, 255, 1)',
+          'rgba(0, 0, 223, 1)',
+          'rgba(0, 0, 191, 1)',
+          'rgba(0, 0, 159, 1)',
+          'rgba(0, 0, 127, 1)',
+          'rgba(63, 0, 91, 1)',
+          'rgba(127, 0, 63, 1)',
+          'rgba(191, 0, 31, 1)',
+          'rgba(255, 0, 0, 1)'
+        ]
+      },
+    }}
+      >
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text={'Sample1'}
+        />
+        <AnyReactComponent
+          lat={59.959100}
+          lng={30.338673}
+          text={'Sample2'}
+        />
+        <AnyReactComponent
+          lat={59.952980}
+          lng={30.332671}
+          text={'Sample3'}
+        />
+      </GoogleMapReact>
+    </div>
+    );
+  }
+}
+export default SimpleMap
