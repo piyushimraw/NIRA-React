16f2cb5c50967bca5ddfbdffde7fad2f7ff7417a
Shadowslayer97 <srirangsathyan17@gmail.com>
2018-03-24 16:27:57 +0530

    Adding heatmap toggle

--- src/Components/map/Map.jsx
+++ src/Components/map/Map.jsx
@@ -7,75 +7,7 @@ import HeatMap from 'google-maps-react';
 
 
 //Trial
-import WithHeatMap from './heatMap';
-
-var gradient = [
-
-      'rgba(0, 255, 255, 0)',
-
-      'rgba(0, 255, 255, 1)',
-
-      'rgba(0, 191, 255, 1)',
-
-      'rgba(0, 127, 255, 1)',
-
-      'rgba(0, 63, 255, 1)',
-
-      'rgba(0, 0, 255, 1)',
-
-      'rgba(0, 0, 223, 1)',
-
-      'rgba(0, 0, 191, 1)',
-
-      'rgba(0, 0, 159, 1)',
-
-      'rgba(0, 0, 127, 1)',
-
-      'rgba(63, 0, 91, 1)',
-
-      'rgba(127, 0, 63, 1)',
-
-      'rgba(191, 0, 31, 1)',
-
-      'rgba(255, 0, 0, 1)'
-
-    ];
-
-    var positions = [
-
-      {lat: 37.782551, lng: -122.445368},
-
-      {lat: 37.782745, lng: -122.444586},
-
-      {lat: 37.782842, lng: -122.443688},
-
-      {lat: 37.782919, lng: -122.442815},
-
-      {lat: 37.782992, lng: -122.442112},
-
-      {lat: 37.783100, lng: -122.441461},
-
-      {lat: 37.783206, lng: -122.440829},
-
-      {lat: 37.783273, lng: -122.440324},
-
-      {lat: 37.783316, lng: -122.440023},
-
-      {lat: 37.783357, lng: -122.439794},
-
-      {lat: 37.783371, lng: -122.439687},
-
-      {lat: 37.783368, lng: -122.439666},
-
-      {lat: 37.783383, lng: -122.439594},
-
-      {lat: 37.783508, lng: -122.439525},
-
-      {lat: 37.783842, lng: -122.439591},
-
-      {lat: 37.784147, lng: -122.439668}
-
-    ];
+//import WithHeatMap from './heatMap';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 const style = {
@@ -107,6 +39,7 @@ export class MapContainer extends Component {
       })
     })
     SampleStore.on('heatmap-clicked',() => {
+      console.log('changing heatmap_loaded');
       this.setState({
         initial_load:false,
         heatmap_load:true,
@@ -214,11 +147,6 @@ render() {
       },
     }}
       >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text={'Kreyser Avrora'}
-        />
       </GoogleMapReact>
     );
   }
